ff03347a6c32cfb379d1b79389493550096b4903
ezmieeline-hub <ezmie.eline@gmail.com>
2026-07-28 21:34:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE='crypto-copilot-v5';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/static/css/app.css','/static/js/app.js']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
